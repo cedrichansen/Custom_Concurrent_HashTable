ae094a28e80fad9ac7a22ba52b0b9acd6c1bc30c
cedrichansen <ceddy29@yahoo.ca>
2018-10-14 20:54:40 -0400

    initializing repo...

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    //https://www.kaggle.com/rtatman/universal-product-code-database
+
+
+    public static void main(String[] args) {
+        System.out.println("hellooooooooo");
+    }
+
+}
